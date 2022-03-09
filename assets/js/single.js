612b53f5cfa22abca4eaba32dc83c87e5a181fe7
Stebbi <stefodd@yahoo.com>
2022-03-09 17:47:55 -0600

    added error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,22 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    // grab repo name form url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    
+    if(repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+        
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -11,16 +28,18 @@ var getRepoIssues = function(repo) {
                 // pass response data to dom function
                 displayIssues(data);
 
+                // check if api has paginated issues
                 if (response.headers.get("Link")) {
                     displayWarning(repo);
                 }
             });
         } else {
-            alert("There was a problem with your request!");
+            // if not successful redirect to the homepage
+            document.location.replace("./index.html");
         }
     });
-    console.log(apiUrl);
 };
+
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
@@ -72,4 +91,5 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+
+getRepoName();
